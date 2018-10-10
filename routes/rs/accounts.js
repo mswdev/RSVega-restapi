3728a5bbb91abb355b031aefd45737b311b61eb6
Sphiinx <sphiinx33@gmail.com>
2018-10-09 19:50:07 -0600

    form-parameters
    - Added node-querybuilder package to easily build queries
    
    - Updated the add account api method
    
    - Updated the update account via id api method

--- routes/rs/accounts.js
+++ routes/rs/accounts.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const mysql = require('mysql')
 const body_parser = require('body-parser')
+const settings = require('./db.json')
+const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool')
 
 const router = express.Router()
 
@@ -9,28 +10,12 @@ router.use(body_parser.urlencoded({
     extended: true
 }))
 
-const pool_connection = mysql.createPool({
-    connectionLimit: 75,
-    host: 'localhost',
-    user: 'nodejs',
-    password: 'naZAus#u%+-F63Susvrt',
-    database: 'rs_accounts',
-    typeCast: function castField(field, useDefaultTypeCasting) {
-        if (field.type === "BIT" && field.length === 1) {
-            const bytes = field.buffer()
-            return (bytes[0] === 1)
-        }
-
-        return (useDefaultTypeCasting())
-    }
-})
-
 router.use(express.static('./public'))
 
 router.get('/rs/accounts/unchecked', (req, res) => {
     const select_account = "SELECT * FROM account WHERE last_update IS NULL LIMIT 1"
 
-    pool_connection.getConnection((error, connection) => {
+    pool.get_connection((error, connection) => {
         if (error) throw error
         connection.query(select_account, (error, results) => {
             if (error) throw error
@@ -45,7 +30,7 @@ router.get('/rs/accounts/:id', (req, res) => {
     let account_id = req.params.id
     const select_account = "SELECT * FROM account WHERE id = ?"
 
-    pool_connection.getConnection((error, connection) => {
+    pool.get_connection((error, connection) => {
         if (error) throw error
         connection.query(select_account, [account_id], (error, results) => {
             if (error) throw error
@@ -57,62 +42,22 @@ router.get('/rs/accounts/:id', (req, res) => {
 })
 
 router.post('/rs/accounts/add', (req, res) => {
-    let username = req.body.username
-    let password = req.body.password
-    const select_account = "SELECT username FROM account WHERE username = ?"
-
-    pool_connection.getConnection((error, connection) => {
-        if (error) throw error
-        connection.query(select_account, [username], (error, results) => {
-            if (error) throw error
-            if (results[0] == null) {
-                const insert_account = "INSERT INTO account (username, password) VALUES (?, ?)"
-                connection.query(insert_account, [username, password], (error, results) => {
-                    if (error) throw error
-                    return res.json(results).end()
-                })
-            } else {
-                return res.status(400).send({
-                    success: false,
-                    message: 'The specified username already exists.'
-                }).end()
-            }
+    pool.get_connection(qb => {
+        qb.insert('account', req.body, true, (err, rows) => {
+            qb.release()
+            if (err) throw err
+            return res.json(rows)
         })
-
-        connection.release()
     })
 })
 
 router.put('/rs/accounts/:id/update', (req, res) => {
-    let account_id = req.params.id
-    let username = req.body.username
-    let password = req.body.password
-    let display_name = req.body.display_name
-    let age = req.body.age
-    let is_members = req.body.is_members
-    let is_bank_pin = req.body.is_bank_pin
-    let is_banned = req.body.is_banned
-    let is_locked = req.body.is_locked
-    let is_auth = req.body.is_auth
-    let is_invalid = req.body.is_invalid
-    const update_account = "UPDATE account SET " +
-        "username = ?, password = ?, display_name = ?," +
-        "age = ?, is_members = b?, is_bank_pin = b?," +
-        "is_banned = b?, is_locked = b?, is_auth = b?," +
-        "is_invalid = b? WHERE id = ?"
-
-    pool_connection.getConnection((error, connection) => {
-        if (error) throw error
-        connection.query(update_account, [username,
-            password, display_name, age,
-            is_members, is_bank_pin, is_banned,
-            is_locked, is_auth, is_invalid,
-            account_id], (error, results) => {
-            if (error) throw error
-            return res.json(results).end()
+    pool.get_connection(qb => {
+        qb.update('account', req.body, {id: req.params.id}, (err, rows) => {
+            qb.release()
+            if (err) throw err
+            return res.json(rows)
         })
-
-        connection.release()
     })
 })
 
@@ -166,7 +111,7 @@ router.put('/rs/accounts/:id/osrs/update', (req, res) => {
         "level_hunter = ?, level_construction = ?, quest_points = ?," +
         "quests_complete = ? WHERE account_id = ?"
 
-    pool_connection.getConnection((error, connection) => {
+    pool.get_connection((error, connection) => {
         if (error) throw error
         connection.query(update_account, [last_ingame,
             bank_worth, inventory_worth, equipment_worth,
@@ -244,7 +189,7 @@ router.put('/rs/accounts/:id/rs3/update', (req, res) => {
         "level_dungeoneering = ?, level_divination = ?, level_invention = ?," +
         "quest_points = ?, quests_complete = ? WHERE account_id = ?"
 
-    pool_connection.getConnection((error, connection) => {
+    pool.get_connection((error, connection) => {
         if (error) throw error
         connection.query(update_account, [last_ingame,
             bank_worth, inventory_worth, equipment_worth,
@@ -269,4 +214,3 @@ router.put('/rs/accounts/:id/rs3/update', (req, res) => {
 })
 
 module.exports = router
-
