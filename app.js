de5fd897742c77eaaabd4eafb1d02fe849d893ab
Matt <sphiinx33@gmail.com>
2019-03-25 02:25:49 -0600

    Preperation for conversion to RSVega

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const cors = require('cors');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.use(cors());
 app.options('*', cors());
 
-const rs_accounts_router = require('./routes/rs/accounts.js')
-app.use(rs_accounts_router)
+const rs_accounts_router = require('./routes/rs/account.js');
+app.use(rs_accounts_router);
 
 app.listen(8080, () => {
     console.log("NodeJS API running on port 8080.")
-})
+});
--- routes/rs/account.js
+++ routes/rs/account.js
@@ -1,75 +1,75 @@
-const express = require('express')
-const body_parser = require('body-parser')
-const settings = require('./db.json')
-const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool')
+const express = require('express');
+const body_parser = require('body-parser');
+const settings = require('./db.json');
+const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool');
 
-const router = express.Router()
+const router = express.Router();
 
-router.use(body_parser.json())
+router.use(body_parser.json());
 router.use(body_parser.urlencoded({
     extended: true
-}))
+}));
 
-router.use(express.static('./public'))
+router.use(express.static('./public'));
 
 router.get('/rs/accounts/unchecked', (req, res) => {
     pool.get_connection(qb => {
         qb.limit(1).order_by("id", "random").get_where('account', {'last_check': null}, (err, rows) => {
-            qb.release()
-            if (err) throw err
+            qb.release();
+            if (err) throw err;
             return res.json(rows)
         })
     })
-})
+});
 
 router.get('/rs/accounts/:id', (req, res) => {
     pool.get_connection(qb => {
         qb.get_where('account', {id: req.params.id}, (err, rows) => {
-            qb.release()
-            if (err) throw err
+            qb.release();
+            if (err) throw err;
             return res.json(rows)
         })
     })
-})
+});
 
 router.post('/rs/accounts/add', (req, res) => {
     pool.get_connection(qb => {
         qb.insert_ignore('account', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
-            qb.release()
-            if (err) throw err
+            qb.release();
+            if (err) throw err;
             return res.json(rows)
         })
     })
-})
+});
 
 router.put('/rs/accounts/:id/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('account', req.body, {id: req.params.id}, (err, rows) => {
-            qb.release()
-            if (err) throw err
+            qb.release();
+            if (err) throw err;
             return res.json(rows)
         })
     })
-})
+});
 
 router.put('/rs/accounts/:id/osrs/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('stats_osrs', req.body, {'account_id': req.params.id}, (err, rows) => {
-            qb.release()
-            if (err) throw err
+            qb.release();
+            if (err) throw err;
             return res.json(rows)
         })
     })
-})
+});
 
 router.put('/rs/accounts/:id/rs3/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('stats_rs3', req.body, {'account_id': req.params.id}, (err, rows) => {
-            qb.release()
-            if (err) throw err
+            qb.release();
+            if (err) throw err;
             return res.json(rows)
         })
     })
-})
+});
 
-module.exports = router
+module.exports = router;
