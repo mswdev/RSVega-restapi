abed0d82bf8e57030e32d7ea29af07ff711ee165
Matt <sphiinx33@gmail.com>
2019-03-25 02:48:46 -0600

    Route Changes
    - Changed account route to bot route naming.
    
    - Added routes for account, session, and mule-order.
    
    - Other conversion changes for RSVega.

---
+++ routes/rs/bot/bot.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const body_parser = require('body-parser');
+const settings = require('../db.json');
+const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool');
+
+const router = express.Router();
+
+router.use(body_parser.json());
+router.use(body_parser.urlencoded({
+    extended: true
+}));
+
+router.use(express.static('./public'));
+
+router.get('/rsvega/bot/unchecked', (req, res) => {
+    pool.get_connection(qb => {
+        qb.limit(1).order_by("id", "random").get_where('bot', {'last_check': null}, (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
+router.get('/rsvega/bot/:id', (req, res) => {
+    pool.get_connection(qb => {
+        qb.get_where('bot', {id: req.params.id}, (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
+router.post('/rsvega/bot/add', (req, res) => {
+    pool.get_connection(qb => {
+        qb.insert_ignore('bot', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
+router.put('/rsvega/bot/:id/update', (req, res) => {
+    pool.get_connection(qb => {
+        qb.update('bot', req.body, {id: req.params.id}, (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
+module.exports = router;
--- routes/rs/bot/mule-order.js
+++ routes/rs/bot/mule-order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const settings = require('./db.json');
+const settings = require('../db.json');
 const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool');
 
 const router = express.Router();
@@ -10,9 +10,9 @@ router.use(body_parser.urlencoded({
     extended: true
 }));
 
-router.get('/rsvega/account/:id', (req, res) => {
+router.post('/rsvega/bot/mule-order/add', (req, res) => {
     pool.get_connection(qb => {
-        qb.get_where('account', {id: req.params.id}, (err, rows) => {
+        qb.insert_ignore('mule_order', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
@@ -20,19 +20,9 @@ router.get('/rsvega/account/:id', (req, res) => {
     })
 });
 
-router.post('/rsvega/account/add', (req, res) => {
+router.put('/rsvega/bot/:id/mule-order/update', (req, res) => {
     pool.get_connection(qb => {
-        qb.insert_ignore('account', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
-            qb.release();
-            if (err) throw err;
-            return res.json(rows)
-        })
-    })
-});
-
-router.put('/rsvega/account/:id/update', (req, res) => {
-    pool.get_connection(qb => {
-        qb.update('account', req.body, {id: req.params.id}, (err, rows) => {
+        qb.update('mule_order', req.body, {id: req.params.id}, (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
