088ead166afbf216b1936e2e7bf2355673350b35
Matt <sphiinx33@gmail.com>
2019-03-27 04:17:42 -0600

    Route get account by username fix.

--- routes/rsvega/account.js
+++ routes/rsvega/account.js
@@ -22,6 +22,7 @@ router.get('/rsvega/account/:id', (req, res) => {
 
 router.get('/rsvega/account/:username', (req, res) => {
     pool.get_connection(qb => {
+        console.log(req.params.username);
         qb.get_where('account', {username: req.params.username}, (err, rows) => {
             qb.release();
             if (err) throw err;
@@ -32,7 +33,7 @@ router.get('/rsvega/account/:username', (req, res) => {
 
 router.post('/rsvega/account/add', (req, res) => {
     pool.get_connection(qb => {
-        qb.insert_ignore('account', req.body, 'ON DUPLICATE KEY UPDATE username=username', (err, rows) => {
+        qb.insert_ignore('account', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
