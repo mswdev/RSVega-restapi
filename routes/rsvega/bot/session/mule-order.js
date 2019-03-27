9611d593917e5772360813411585c596feaeb2a4
Matt <sphiinx33@gmail.com>
2019-03-27 00:25:55 -0600

    Fixed routing for mule-order

--- routes/rsvega/bot/session/mule-order.js
+++ routes/rsvega/bot/session/mule-order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const settings = require('../db.json');
+const settings = require('../../db.json');
 const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool');
 
 const router = express.Router();
@@ -10,7 +10,7 @@ router.use(body_parser.urlencoded({
     extended: true
 }));
 
-router.post('/rsvega/bot/mule-order/add', (req, res) => {
+router.post('/rsvega/bot/session/mule-order/add', (req, res) => {
     pool.get_connection(qb => {
         qb.insert_ignore('mule_order', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
             qb.release();
@@ -20,7 +20,7 @@ router.post('/rsvega/bot/mule-order/add', (req, res) => {
     })
 });
 
-router.put('/rsvega/bot/:id/mule-order/update', (req, res) => {
+router.put('/rsvega/bot/session/:id/mule-order/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('mule_order', req.body, {id: req.params.id}, (err, rows) => {
             qb.release();
