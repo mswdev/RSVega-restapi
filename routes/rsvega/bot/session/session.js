b1c499aee48a9fda1cb53281a774a99e679bd16c
Matt <sphiinx33@gmail.com>
2019-03-28 02:00:40 -0600

    Added session route to get newest session from a specific bot id.

--- routes/rsvega/bot/session/session.js
+++ routes/rsvega/bot/session/session.js
@@ -10,6 +10,16 @@ router.use(body_parser.urlencoded({
     extended: true
 }));
 
+router.get('/rsvega/bot/session/bot-id/:bot_id/newest', (req, res) => {
+    pool.get_connection(qb => {
+        qb.limit(1).order_by("id", "ASC").get_where('session', {id: req.params.bot_id}, (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
 router.post('/rsvega/bot/session/add', (req, res) => {
     pool.get_connection(qb => {
         qb.insert_ignore('session', req.body, 'ON DUPLICATE KEY UPDATE id=id', (err, rows) => {
