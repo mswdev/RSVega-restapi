212c5d76d56d30af315204c6ba936eceef3f620a
Matt <sphiinx33@gmail.com>
2019-03-30 03:28:44 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const request = require('request');
 const axios = require('axios');
 
 const router = express.Router();
@@ -11,21 +10,18 @@ router.use(body_parser.urlencoded({
 }));
 
 router.get('/rsvega/bot/create', (req, res) => {
-        axios.get('https://api.sphiinx.me/rsvega/bot/id/1')
-            .then(function (response) {
-                // handle success
-                console.log(response.data[1]);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+    axios.post('http://2captcha.com/in.php', {
+        json: '1',
+        key: 'fe920f0af037e534bb8180f0dbdec403',
+        method: 'userrecaptcha',
+        googlekey: '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv',
+        pageurl: 'https://secure.runescape.com/m=account-creation/create_account?theme=oldschool',
+    }).then(function (response) {
+        console.log(response.data)
+    }).catch(function (error) {
+        console.log(error);
+    })
 });
-
-
 
 
 module.exports = router;
