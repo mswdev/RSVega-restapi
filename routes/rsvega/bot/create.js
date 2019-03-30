44ef85ff436b18995ab088e9681378c00553f319
Matt <sphiinx33@gmail.com>
2019-03-30 03:50:33 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -4,28 +4,43 @@ const axios = require('axios');
 
 const router = express.Router();
 
+const captcha_api_key = fe920f0af037e534bb8180f0dbdec403;
+
 router.use(body_parser.json());
 router.use(body_parser.urlencoded({
     extended: true
 }));
 
 router.get('/rsvega/bot/create', (req, res) => {
+    console.log(setTimeout(getCaptchaKey(postCaptchaKey()), 35000))
+});
+
+function postCaptchaKey() {
     axios.post('http://2captcha.com/in.php', {
         json: '1',
-        key: 'fe920f0af037e534bb8180f0dbdec403',
+        key: captcha_api_key,
         method: 'userrecaptcha',
         googlekey: '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv',
         pageurl: 'https://secure.runescape.com/m=account-creation/create_account?theme=oldschool',
     }).then(function (response) {
-        console.log(response.data.request)
+        return response.data.request;
     }).catch(function (error) {
-        console.log(error);
+        return error;
     })
-});
-
-function postCaptcha() {
-
 }
 
+function getCaptchaKey(request_id) {
+    axios.post('http://2captcha.com/res.php', {
+        json: '1',
+        id: request_id,
+        key: captcha_api_key,
+        action: get,
+    }).then(function (response) {
+        console.log(response.data.request);
+        return response.data.request;
+    }).catch(function (error) {
+        return error;
+    })
+}
 
 module.exports = router;
