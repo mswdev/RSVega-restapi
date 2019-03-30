ddf9147200f6984083e090323293148f9865ee8e
Matt <sphiinx33@gmail.com>
2019-03-30 15:17:39 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const axios = require('axios');
+var solver = require('2captcha')
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.use(body_parser.urlencoded({
 }));
 
 router.get('/rsvega/bot/create', (req, res) => {
-    console.log(SolveRecaptchaV2(captcha_api_key, '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv', create_bot_url))
+    console.log(solver.decodeRecaptchaV2('6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv', create_bot_url))
     /*postCaptchaID()
         .then(function (response) {
             setTimeout(function () {
@@ -33,65 +34,6 @@ router.get('/rsvega/bot/create', (req, res) => {
         return error
     });*/
 });
-
-function SolveRecaptchaV2(APIKey, googleKey, pageUrl, proxy, proxyType){
-    var requestUrl = "http://2captcha.com/in.php?key=" + APIKey + "&method=userrecaptcha&googlekey=" + googleKey + "&pageurl=" + pageUrl;
-
-    switch (proxyType) {
-        case 'HTTP':
-            requestUrl = requestUrl + "HTTP";
-            break;
-
-        case 'HTTPS':
-            requestUrl = requestUrl + "HTTPS";
-            break;
-
-        case 'SOCKS4':
-            requestUrl = requestUrl + "SOCKS4";
-            break;
-
-        case 'SOCKS5':
-            requestUrl = requestUrl + "SOCKS5";
-            break;
-    }
-
-    $.ajax({url: "requestUrl", success: async function (result) {
-            if (result.length < 3) {
-                return false;
-            } else {
-                if (result.substring(0, 3) === "OK|") {
-                    var captchaID = result.substring(3);
-
-                    for (var i = 0; i < 24; i++) {
-                        var ansUrl = "http://2captcha.com/res.php?key=" + APIKey + "&action=get&id=" + captchaID;
-
-                        $.ajax({
-                            url: ansUrl, success: function (ansresult) {
-                                if (ansresult.length < 3) {
-                                    return ansresult;
-                                } else {
-                                    if (ansresult.substring(0, 3) === "OK|") {
-                                        return ansresult;
-                                    } else if (ansresult !== "CAPCHA_NOT_READY") {
-                                        return ansresult;
-                                    }
-                                }
-                            }
-                        });
-                        await sleep(5000);
-                    }
-
-                } else {
-                    return ansresult;
-                }
-            }
-        },
-        fail: function(){
-            return "";
-        }
-    });
-
-}
 
 function postCaptchaID() {
     return axios.post('http://2captcha.com/in.php', {
