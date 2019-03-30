9c91bccf191a8c070ac7ec1afc0736bd0c19b6a2
Matt <sphiinx33@gmail.com>
2019-03-30 03:51:26 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -4,8 +4,6 @@ const axios = require('axios');
 
 const router = express.Router();
 
-const captcha_api_key = fe920f0af037e534bb8180f0dbdec403;
-
 router.use(body_parser.json());
 router.use(body_parser.urlencoded({
     extended: true
@@ -18,7 +16,7 @@ router.get('/rsvega/bot/create', (req, res) => {
 function postCaptchaKey() {
     axios.post('http://2captcha.com/in.php', {
         json: '1',
-        key: captcha_api_key,
+        key: 'fe920f0af037e534bb8180f0dbdec403',
         method: 'userrecaptcha',
         googlekey: '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv',
         pageurl: 'https://secure.runescape.com/m=account-creation/create_account?theme=oldschool',
@@ -33,7 +31,7 @@ function getCaptchaKey(request_id) {
     axios.post('http://2captcha.com/res.php', {
         json: '1',
         id: request_id,
-        key: captcha_api_key,
+        key: 'fe920f0af037e534bb8180f0dbdec403',
         action: get,
     }).then(function (response) {
         console.log(response.data.request);
