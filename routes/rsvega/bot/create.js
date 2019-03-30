b32716fdd6ba0a6e386f81167046cf5c7cdf4c70
Matt <sphiinx33@gmail.com>
2019-03-30 01:47:48 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -14,7 +14,7 @@ router.get('/rsvega/bot/create', (req, res) => {
         axios.get('https://api.sphiinx.me/rsvega/bot/id/1')
             .then(function (response) {
                 // handle success
-                console.log(response);
+                console.log(response.body);
             })
             .catch(function (error) {
                 // handle error
