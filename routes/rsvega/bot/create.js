090d88c14c59b78043d17b51b118142a6c0d4b42
Matt <sphiinx33@gmail.com>
2019-03-30 14:08:34 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const router = express.Router();
 
 const captcha_api_key = 'fe920f0af037e534bb8180f0dbdec403';
+const create_bot_url = 'https://secure.runescape.com/m=account-creation/create_account';
 
 router.use(body_parser.json());
 router.use(body_parser.urlencoded({
@@ -14,7 +15,14 @@ router.use(body_parser.urlencoded({
 router.get('/rsvega/bot/create', (req, res) => {
     postCaptchaID()
         .then(function (response) {
-            return parseCaptchaID(response.data.request)
+            setTimeout(function () {
+                getCaptchaKey(response.data.request)
+                    .then(function (response) {
+                        return res.json(response.data.request)
+                    }).catch(function (error) {
+                    return error
+                })
+            }, 35000)
         }).catch(function (error) {
         return error
     });
@@ -22,34 +30,37 @@ router.get('/rsvega/bot/create', (req, res) => {
 
 function postCaptchaID() {
     return axios.post('http://2captcha.com/in.php', {
-        json: '1',
-        key: captcha_api_key,
-        method: 'userrecaptcha',
-        googlekey: '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv',
-        pageurl: 'https://secure.runescape.com/m=account-creation/create_account?theme=oldschool',
+        'json': '1',
+        'key': captcha_api_key,
+        'method': 'userrecaptcha',
+        'googlekey': '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv',
+        'pageurl': create_bot_url,
     })
-}
-
-function parseCaptchaID(request_id) {
-    setTimeout(function () {
-        getCaptchaKey(request_id)
-            .then(function (response) {
-                return res.json(response.data.request)
-            }).catch(function (error) {
-            return error
-        })
-    }, 35000)
 }
 
 function getCaptchaKey(request_id) {
     return axios.get('http://2captcha.com/res.php', {
         params: {
-            json: '1',
-            id: request_id,
-            key: captcha_api_key,
-            action: 'get',
+            'json': '1',
+            'id': request_id,
+            'key': captcha_api_key,
+            'action': 'get',
         }
     })
+}
+
+function postCreateBot(captcha_key) {
+    return axios.post(create_bot_url), {
+        email1: 'rspeerdev95@gmail.com',
+        onlyOneEmail: '1',
+        password1: 'Killkid5',
+        onlyOnePassword: '1',
+        day: '27',
+        month: '07',
+        year: '1998',
+        'create-submit': 'create',
+        'g-recaptcha-response': captcha_key,
+    }
 }
 
 module.exports = router;
