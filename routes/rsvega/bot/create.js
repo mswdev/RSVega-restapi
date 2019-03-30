15f9054395575b6b682b4cef11a756d56e673daa
Matt <sphiinx33@gmail.com>
2019-03-30 04:11:18 -0600

    Test for bot cration support

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -12,13 +12,31 @@ router.use(body_parser.urlencoded({
 }));
 
 router.get('/rsvega/bot/create', (req, res) => {
-    axios.get('https://api.sphiinx.me/rsvega/bot/id/1').then(function (response) {
-        console.log(response.data);
-        return response.data.username;
+   getCaptchaKey()
+});
+
+function postCaptchaKey() {
+    axios.post('http://2captcha.com/in.php', {
+        json: '1',
+        key: captcha_api_key,
+        method: 'userrecaptcha',
+        googlekey: '6Lcsv3oUAAAAAGFhlKrkRb029OHio098bbeyi_Hv',
+        pageurl: 'https://secure.runescape.com/m=account-creation/create_account?theme=oldschool',
+    }).then(function (response) {
+        console.log(response.data.request)
+        return response.data.request;
     }).catch(function (error) {
         return error;
     })
-});
+}
 
+function getCaptchaKey(request_id) {
+    axios.get('http://2captcha.com/res.php?json=1&id=61352700903&key=fe920f0af037e534bb8180f0dbdec403&action=get').then(function (response) {
+        console.log(response.data.request);
+        return response.data.request;
+    }).catch(function (error) {
+        return error;
+    })
+}
 
 module.exports = router;
