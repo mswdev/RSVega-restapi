dca1a3fdb8f69650d9ad3e33fc91d35249366761
Sphiinx <sphiinx33@gmail.com>
2019-04-10 06:17:38 -0700

    Removed unused code

--- routes/rsvega/bot/create.js
+++ routes/rsvega/bot/create.js
@@ -11,7 +11,7 @@ router.use(body_parser.urlencoded({
 
 router.post('/rsvega/bot/create', (req, res) => {
     account.create(req.body.two_captcha_api_key, req.body.email, req.body.password, req.body.socks_ip, req.body.socks_port, req.body.socks_username, req.body.socks_password).then(response => {
-        return res(response);
+        return res.send(response);
     })
 });
 
