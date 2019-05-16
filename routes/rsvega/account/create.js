a62e547460732c1a11c6cb7df7011d65de4fdffe
Sphiinx <sphiinx33@gmail.com>
2019-05-16 03:01:16 -0600

    Upgrades
    - Updated runescape-account-creator npm package to the newest version.

--- routes/rsvega/account/create.js
+++ routes/rsvega/account/create.js
@@ -9,10 +9,33 @@ router.use(body_parser.urlencoded({
     extended: true
 }));
 
-router.post('/rsvega/account/create', (req, res) => {
-    account.create(req.body.two_captcha_api_key, req.body.email, req.body.password, req.body.socks_ip, req.body.socks_port, req.body.socks_username, req.body.socks_password).then(response => {
-        return res.send(response);
-    })
+router.post('/rsvega/account/create', async (req, res) => {
+    const builder = account.buildAccountCreator(req.body.two_captcha_api_key);
+
+    try {
+        const response = await builder.register({
+            email: req.body.email,
+            password: req.body.password,
+            proxy: formatProxyUrl(req.body.socks_ip, req.body.socks_port, req.body.socks_username, req.body.socks_password)
+        });
+
+        res.send(response);
+    } catch (e) {
+        console.error(e);
+        res.send({error: e.message}).status(500);
+    }
 });
+
+function formatProxyUrl(ip, port, username, password) {
+    if (!ip || !port) {
+        return null
+    }
+
+    if (!username && !password) {
+        return `socks5://${ip}:${port}`
+    }
+
+    return `socks5://${username}:${password}@${ip}:${port}`
+}
 
 module.exports = router;
