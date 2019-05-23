fdb4af382d8a221fc6e886cc385a64a5a4c716b4
Sphiinx <sphiinx33@gmail.com>
2019-05-23 15:10:31 -0600

    Endpoints
    - Added endpoints for getting bot stats by id.

--- routes/rsvega/bot/stats.js
+++ routes/rsvega/bot/stats.js
@@ -10,6 +10,26 @@ router.use(body_parser.urlencoded({
     extended: true
 }));
 
+router.get('/rsvega/bot/id/:id/stats/osrs', (req, res) => {
+    pool.get_connection(qb => {
+        qb.get_where('bot', {id: req.params.id}, (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
+router.get('/rsvega/bot/id/:id/stats/rs3', (req, res) => {
+    pool.get_connection(qb => {
+        qb.get_where('bot', {id: req.params.id}, (err, rows) => {
+            qb.release();
+            if (err) throw err;
+            return res.json(rows)
+        })
+    })
+});
+
 router.put('/rsvega/bot/id/:id/stats/osrs/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('stats_osrs', req.body, {'bot_id': req.params.id}, (err, rows) => {
