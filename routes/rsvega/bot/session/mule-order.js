773cfbd34f1fd1071e335b6305f2e63de3a1dbd1
Sphiinx <sphiinx33@gmail.com>
2019-05-23 00:23:40 -0600

    Bug Fixes
    - Fixed endpoint path

--- routes/rsvega/bot/session/mule-order.js
+++ routes/rsvega/bot/session/mule-order.js
@@ -10,9 +10,9 @@ router.use(body_parser.urlencoded({
     extended: true
 }));
 
-router.get('/rsvega/bot/session/account-id/:account_id/mule-order/newest', (req, res) => {
+router.get('/rsvega/bot/session/bot-id/:bot_id/mule-order/newest', (req, res) => {
     pool.get_connection(qb => {
-        qb.limit(1).order_by("id", "DESC").get_where('mule_order', {account_id: req.params.account_id}, (err, rows) => {
+        qb.limit(1).order_by("id", "DESC").get_where('mule_order', {bot_id: req.params.bot_id}, (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
