c7dfa82849e4991498c3a78c458132fe44d59254
Sphiinx <sphiinx33@gmail.com>
2019-05-23 15:34:52 -0600

    Forgot to change table

--- routes/rsvega/bot/stats.js
+++ routes/rsvega/bot/stats.js
@@ -12,7 +12,7 @@ router.use(body_parser.urlencoded({
 
 router.get('/rsvega/bot/id/:id/stats/osrs', (req, res) => {
     pool.get_connection(qb => {
-        qb.get_where('bot', {id: req.params.id}, (err, rows) => {
+        qb.get_where('stats_osrs', {bot_id: req.params.id}, (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
@@ -22,7 +22,7 @@ router.get('/rsvega/bot/id/:id/stats/osrs', (req, res) => {
 
 router.get('/rsvega/bot/id/:id/stats/rs3', (req, res) => {
     pool.get_connection(qb => {
-        qb.get_where('bot', {id: req.params.id}, (err, rows) => {
+        qb.get_where('stats_rs3', {bot_id: req.params.id}, (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
@@ -32,7 +32,7 @@ router.get('/rsvega/bot/id/:id/stats/rs3', (req, res) => {
 
 router.put('/rsvega/bot/id/:id/stats/osrs/update', (req, res) => {
     pool.get_connection(qb => {
-        qb.update('stats_osrs', req.body, {'bot_id': req.params.id}, (err, rows) => {
+        qb.update('stats_osrs', req.body, {bot_id: req.params.id}, (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
@@ -42,7 +42,7 @@ router.put('/rsvega/bot/id/:id/stats/osrs/update', (req, res) => {
 
 router.put('/rsvega/bot/id/:id/stats/rs3/update', (req, res) => {
     pool.get_connection(qb => {
-        qb.update('stats_rs3', req.body, {'bot_id': req.params.id}, (err, rows) => {
+        qb.update('stats_rs3', req.body, {bot_id: req.params.id}, (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
