c9d9d8d4e935352922aea598e2d71483b4cce4ac
Sphiinx <sphiinx33@gmail.com>
2019-06-07 05:13:03 -0600

    Remove time conversion

--- routes/account/account.js
+++ routes/account/account.js
@@ -59,7 +59,7 @@ router.get('/rsvega/account/is-mule/active/random', (req, res) => {
 // Get account where user id and is mule order by random limit 1
 router.get('/rsvega/account/user-id/:user_id/is-mule/active/random', (req, res) => {
     pool.get_connection(qb => {
-        qb.query('SELECT * FROM `account` WHERE `is_mule` = 1 AND `last_update` AND `user_id` = ' + req.params.user_id + ' >= CONVERT_TZ(now(),\'Europe/London\',\'US/Mountain\') - INTERVAL 1 MINUTE ORDER BY RAND() LIMIT 1', (err, rows) => {
+        qb.query('SELECT * FROM `account` WHERE `is_mule` = 1 AND `last_update` AND `user_id` = ' + qb.escape(req.params.user_id) + ' >= NOW() - INTERVAL 1 MINUTE ORDER BY RAND() LIMIT 1', (err, rows) => {
             qb.release();
             if (err) throw err;
             return res.json(rows)
