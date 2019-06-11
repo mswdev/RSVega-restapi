9fea26f6fc8602883c1bfb9824484cf84f3db579
Sphiinx <sphiinx33@gmail.com>
2019-06-10 22:23:49 -0600

    Improvments
    - Removed semi-colons.
    - Added debug npm package.
    - Added debug messages to each endpoint.
    - Removed error throwing from each endpoint.
    - Each endpoint now returns the error.

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
-app.use(cors());
-app.options('*', cors());
+const cors = require('cors')
+const express = require('express')
+const app = express()
+app.use(cors())
+app.options('*', cors())
 
-const user_router = require('./routes/user/user.js');
-const system_info_router = require('./routes/user/system-info.js');
-const account_router = require('./routes/account/account.js');
-const create_account_router = require('./routes/account/create.js');
-const stats_router = require('./routes/account/stats/stats.js');
-const session_router = require('./routes/account/session/session.js');
-const session_position_router = require('./routes/account/session/session_position.js');
-const mule_order_router = require('./routes/account/session/mule-order.js');
-app.use(user_router, system_info_router, account_router, create_account_router, stats_router, session_router, session_position_router, mule_order_router);
+const user_router = require('./routes/user/user.js')
+const system_info_router = require('./routes/user/system-info.js')
+const account_router = require('./routes/account/account.js')
+const create_account_router = require('./routes/account/create.js')
+const stats_router = require('./routes/account/stats/stats.js')
+const session_router = require('./routes/account/session/session.js')
+const session_position_router = require('./routes/account/session/session_position.js')
+const mule_order_router = require('./routes/account/session/mule-order.js')
+app.use(user_router, system_info_router, account_router, create_account_router, stats_router, session_router, session_position_router, mule_order_router)
 
 app.listen(8080, () => {
     console.log("NodeJS API running on port 8080.")
-});
+})
--- routes/account/stats/stats.js
+++ routes/account/stats/stats.js
@@ -1,57 +1,74 @@
-const express = require('express');
-const body_parser = require('body-parser');
-const settings = require('../../db.json');
-const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool');
+const express = require('express')
+const body_parser = require('body-parser')
+const settings = require('../../db.json')
+const pool = require('node-querybuilder').QueryBuilder(settings, 'mysql', 'pool')
+const debug = require('debug')('rsvega-restapi:stats')
 
-const router = express.Router();
+const router = express.Router()
 
-router.use(body_parser.json());
+router.use(body_parser.json())
 router.use(body_parser.urlencoded({
     extended: true
-}));
+}))
 
 // Get stats osrs by account id
 router.get('/rsvega/account/id/:id/stats/osrs', (req, res) => {
     pool.get_connection(qb => {
         qb.get_where('stats_osrs', {account_id: req.params.id}, (err, rows) => {
-            qb.release();
-            if (err) throw err;
-            return res.json(rows)
+            qb.release()
+            if (err) {
+                debug(err)
+                return res.json([err])
+            }
+
+            return res.json([rows])
         })
     })
-});
+})
 
 // Get stats rs3 by account id
 router.get('/rsvega/account/id/:id/stats/rs3', (req, res) => {
     pool.get_connection(qb => {
         qb.get_where('stats_rs3', {account_id: req.params.id}, (err, rows) => {
-            qb.release();
-            if (err) throw err;
-            return res.json(rows)
+            qb.release()
+            if (err) {
+                debug(err)
+                return res.json([err])
+            }
+
+            return res.json([rows])
         })
     })
-});
+})
 
 // Update stats osrs by account id
 router.put('/rsvega/account/id/:id/stats/osrs/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('stats_osrs', req.body, {account_id: req.params.id}, (err, rows) => {
-            qb.release();
-            if (err) throw err;
+            qb.release()
+            if (err) {
+                debug(err)
+                return res.json([err])
+            }
+
             return res.json([rows])
         })
     })
-});
+})
 
 // Update stats rs3 by account id
 router.put('/rsvega/account/id/:id/stats/rs3/update', (req, res) => {
     pool.get_connection(qb => {
         qb.update('stats_rs3', req.body, {account_id: req.params.id}, (err, rows) => {
-            qb.release();
-            if (err) throw err;
+            qb.release()
+            if (err) {
+                debug(err)
+                return res.json([err])
+            }
+
             return res.json([rows])
         })
     })
-});
+})
 
-module.exports = router;
+module.exports = router
